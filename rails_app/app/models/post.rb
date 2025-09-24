e2dd7baca85e3750b8289a4dfe27d3d0594d5301
Luis Cuevas <lcuevas@tirant.com>
2025-09-24 03:00:20 -0500

    chore: remove Rails app configuration files and boilerplate code

--- rails_app/app/models/post.rb
+++
@@ -1,8 +0,0 @@
-class Post < ApplicationRecord
-  belongs_to :user
-  validates :title, presence: true
-  validates :content, presence: true
-
-  scope :published, -> { where(published: true) }
-  scope :recent, -> { order(created_at: :desc) }
-end
